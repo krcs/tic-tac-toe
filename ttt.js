f1b1b66a26bd240606f48a1c15f787964c09496a
kc <k.netpost@gmail.com>
2022-08-03 19:30:43 +0200

    Check if WebAssembly is supported.

--- ttt.js
+++ ttt.js
@@ -177,14 +177,32 @@ class Game {
             element.innerHTML = "";
     }
 }
+// https://stackoverflow.com/questions/47879864/how-can-i-check-if-a-browser-supports-webassembly#47880734
+function isWebAssembly() {
+     try {
+        if (typeof WebAssembly === "object"
+            && typeof WebAssembly.instantiate === "function") {
+            const module = new WebAssembly.Module(Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00));
+            if (module instanceof WebAssembly.Module)
+                return new WebAssembly.Instance(module) instanceof WebAssembly.Instance;
+        }
+    } catch (e) {
+    }
+    return false;
+}
 
 const main = async () => {
+    const info = document.getElementById("info");
+    if (!isWebAssembly()) {
+        info.innerHTML("WebAssembly is not supported.");
+        return;
+    }
     const tttai = new TTTAI("ttt.wasm");
     await tttai.initialize();
 
     const startButton = document.getElementById("start");
     const game = new Game(tttai);
-    game.setInfoElement(document.getElementById("info"));
+    game.setInfoElement(info);
     game.setBoardElements([
         document.getElementById("c001"),
         document.getElementById("c002"),
